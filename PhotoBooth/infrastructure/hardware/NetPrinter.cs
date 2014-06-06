5a44eebb9bb36d03578c844e2b623db2e8a9be58
unknown <nly96192@NLVEHVRES2NBZC7.code1.emi.philips.com>
2014-06-06 23:20:34 +0200

    another try after another empty page

--- PhotoBooth/infrastructure/hardware/NetPrinter.cs
+++ PhotoBooth/infrastructure/hardware/NetPrinter.cs
@@ -35,6 +35,7 @@ namespace com.prodg.photobooth.infrastructure.hardware
         private readonly ISettings settings;
         private readonly ILogger logger;
         private Image storedImage;
+        private PrintDocument pd;
 
         public NetPrinter(ISettings settings, ILogger logger)
         {
@@ -51,29 +52,30 @@ namespace com.prodg.photobooth.infrastructure.hardware
             try
             {
                 storedImage = image;
-                using (var pd = new PrintDocument())
+                pd = new PrintDocument();
+
+                pd.PrintPage += printDocument_PrintPage2;
+                pd.BeginPrint += printDocument_BeginPrint2;
+                pd.PrinterSettings.PrinterName = settings.PrinterName;
+                //Set the paper settings before calling print in order to get the correct graphics object
+
+                // We ALWAYS want true here, as we will implement the 
+                // margin limitations later in code.
+                pd.OriginAtMargins = true;
+
+                pd.DefaultPageSettings.PrinterResolution = new PrinterResolution
                 {
-                    pd.PrintPage += printDocument_PrintPage;
-                    pd.PrinterSettings.PrinterName = settings.PrinterName;
-                    //Set the paper settings before calling print in order to get the correct graphics object
+                    X = 300,
+                    Y = 300,
+                    Kind = PrinterResolutionKind.High
+                };
+                //Pick the first papersize
+                pd.DefaultPageSettings.PaperSize = pd.PrinterSettings.PaperSizes[0];
+                //pd.DefaultPageSettings.Landscape = true;
+                //pd.DefaultPageSettings.Margins = new Margins(0, 0, 0, 0);
 
-                    // We ALWAYS want true here, as we will implement the 
-                    // margin limitations later in code.
-                    pd.OriginAtMargins = true;
+                pd.Print();
 
-                    pd.DefaultPageSettings.PrinterResolution = new PrinterResolution
-                    {
-                        X = 300,
-                        Y = 300,
-                        Kind = PrinterResolutionKind.High
-                    };
-                    //Pick the first papersize
-                    pd.DefaultPageSettings.PaperSize = pd.PrinterSettings.PaperSizes[0];
-                    pd.DefaultPageSettings.Landscape = true;
-                    pd.DefaultPageSettings.Margins = new Margins(0, 0, 0, 0);
-
-                    pd.Print();
-                }
             }
             catch (Exception ex)
             {
@@ -250,6 +252,76 @@ namespace com.prodg.photobooth.infrastructure.hardware
 
             e.Graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
             e.Graphics.DrawImage(storedImage, printedPrintableArea);
+        }
+
+        private void printDocument_BeginPrint2(object sender, PrintEventArgs e)
+        {
+            // Save our print action so we know if we are printing 
+            // a preview or a real document.
+            printAction = e.PrintAction;
+
+            // Set some preferences, our method should print a box with any 
+            // combination of these properties being true/false.
+            pd.OriginAtMargins = false;   //true = soft margins, false = hard margins
+            pd.DefaultPageSettings.Landscape = true;
+        }
+
+        private void printDocument_PrintPage2(object sender, PrintPageEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            // If you set printDocumet.OriginAtMargins to 'false' this event 
+            // will print the largest rectangle your printer is physically 
+            // capable of. This is often 1/8" - 1/4" from each page edge.
+            // ----------
+            // If you set printDocument.OriginAtMargins to 'false' this event
+            // will print the largest rectangle permitted by the currently 
+            // configured page margins. By default the page margins are 
+            // usually 1" from each page edge but can be configured by the end
+            // user or overridden in your code.
+            // (ex: printDocument.DefaultPageSettings.Margins)
+
+            // Grab a copy of our "soft margins" (configured printer settings)
+            // Defaults to 1 inch margins, but could be configured otherwise by 
+            // the end user. You can also specify some default page margins in 
+            // your printDocument.DefaultPageSetting properties.
+            RectangleF marginBounds = e.MarginBounds;
+
+            // Grab a copy of our "hard margins" (printer's capabilities) 
+            // This varies between printer models. Software printers like 
+            // CutePDF will have no "physical limitations" and so will return 
+            // the full page size 850,1100 for a letter page size.
+            RectangleF printableArea = e.PageSettings.PrintableArea;
+
+            // If we are print to a print preview control, the origin won't have 
+            // been automatically adjusted for the printer's physical limitations. 
+            // So let's adjust the origin for preview to reflect the printer's 
+            // hard margins.
+            if (printAction == PrintAction.PrintToPreview)
+                g.TranslateTransform(printableArea.X, printableArea.Y);
+
+            // Are we using soft margins or hard margins? Lets grab the correct 
+            // width/height from either the soft/hard margin rectangles. The 
+            // hard margins are usually a little wider than the soft margins.
+            // ----------
+            // Note: Margins are automatically applied to the rotated page size 
+            // when the page is set to landscape, but physical hard margins are 
+            // not (the printer is not physically rotating any mechanics inside, 
+            // the paper still travels through the printer the same way. So we 
+            // rotate in software for landscape)
+            int availableWidth = (int)Math.Floor(pd.OriginAtMargins ? marginBounds.Width : (e.PageSettings.Landscape ? printableArea.Height : printableArea.Width));
+            int availableHeight = (int)Math.Floor(pd.OriginAtMargins ? marginBounds.Height : (e.PageSettings.Landscape ? printableArea.Width : printableArea.Height));
+
+            // Draw our rectangle which will either be the soft margin rectangle 
+            // or the hard margin (printer capabilities) rectangle.
+            // ----------
+            // Note: we adjust the width and height minus one as it is a zero, 
+            // zero based co-ordinates system. This will put the rectangle just 
+            // inside the available width and height.
+            g.DrawRectangle(Pens.Red, 0, 0, availableWidth - 1, availableHeight - 1);
+
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g.DrawImage(storedImage, printableArea);
         }
     }
 }
