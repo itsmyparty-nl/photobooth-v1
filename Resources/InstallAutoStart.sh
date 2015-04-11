a81a21371e2bcc3f2f7fee0a58ce6a3d5563b59d
Patrick Bronneberg <p.bronneberg@gmail.com>
2015-04-11 11:11:32 +0000

    Updated autostart script

--- Resources/InstallAutoStart.sh
+++ Resources/InstallAutoStart.sh
@@ -3,3 +3,5 @@ echo creating autostart script folder
 mkdir ~/.config/autostart
 echo Installing photobooth autostart script
 cp photobooth.desktop ~/.config/autostart
+echo Copying autostart shell script to user folder
+cp StartPhotoBooth.sh ~/
