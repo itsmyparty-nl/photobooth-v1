2554d557102622ce0764bc682ec7e49a846302b9
Patrick Bronneberg <p.bronneberg@gmail.com>
2015-04-13 16:45:32 +0200

    Added missing file

---
+++ PhotoBooth/domain/converters/ImageConverter.cs
@@ -0,0 +1,49 @@
+﻿#region PhotoBooth - MIT - (c) 2015 Patrick Bronneberg
+/*
+  PhotoBooth - an application to control a DIY photobooth
+
+  Permission is hereby granted, free of charge, to any person obtaining
+  a copy of this software and associated documentation files (the
+  "Software"), to deal in the Software without restriction, including
+  without limitation the rights to use, copy, modify, merge, publish,
+  distribute, sublicense, and/or sell copies of the Software, and to
+  permit persons to whom the Software is furnished to do so, subject to
+  the following conditions:
+
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+  
+  Copyright 2014 Patrick Bronneberg
+*/
+#endregion
+
+using System;
+using System.Drawing;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace com.prodg.photobooth.domain.converters
+{
+    public class ImageConverter : Newtonsoft.Json.JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(Image);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var m = new MemoryStream(Convert.FromBase64String((string)reader.Value));
+            return Image.FromStream(m);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var bmp = (Image)value;
+            var m = new MemoryStream();
+            bmp.Save(m, System.Drawing.Imaging.ImageFormat.Png);
+
+            writer.WriteValue(Convert.ToBase64String(m.ToArray()));
+        }
+    }
+}
