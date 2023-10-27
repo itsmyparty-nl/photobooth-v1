6bd34d2904db7c635c4dd3eec7658500914f050a
Patrick Bronneberg <p.bronneberg@gmail.com>
2023-10-27 19:41:15 +0200

    GUI works. Run all picture modification on the UI thread for now

--- PhotoBoothGUI/MainWindow.cs
+++ PhotoBoothGUI/MainWindow.cs
@@ -221,8 +221,9 @@ public partial class MainWindow: Gtk.Window
 	    return false;
 	}
 
-	private void OnPhotoBoothServicePictureAdded (object sender, PictureAddedEventArgs a)
+	private async void OnPhotoBoothServicePictureAdded (object sender, PictureAddedEventArgs a)
 	{
+		_logger.LogInformation($"Received picture {a.Index}");
 		Gtk.Application.Invoke ((b, c) => {
 			if (a.IsFinal) {
 				imageInstruction.Pixbuf = instructionImages ["finished"];
@@ -233,15 +234,17 @@ public partial class MainWindow: Gtk.Window
 			var result = CreateAndScalePicture (a.Picture, imagePhoto.Allocation.Height);
 
 			//Set the pixbuf on the UI
-			imagePhoto.Pixbuf = result.Result;
+			imagePhoto.Pixbuf = result;
 			ShowAll ();
 		});
 	}
 
-	private async Task<Gdk.Pixbuf> CreateAndScalePicture(SixLabors.ImageSharp.Image picture, int height)
+	private Gdk.Pixbuf CreateAndScalePicture(SixLabors.ImageSharp.Image picture, int height)
     {
-		return await Task.Run(() =>
-	    {
+		_logger.LogInformation($"Enter CreateAndScale picture height `{height}");
+
+		// return await Task.Run(() =>
+	    // {
 	        using (var stream = new MemoryStream())
 	        {
 		        picture.Save(stream, new BmpEncoder());
@@ -251,7 +254,7 @@ public partial class MainWindow: Gtk.Window
 	            double scale = height/(double) pixBuf.Height;
                 return pixBuf.ScaleSimple((int)(scale * pixBuf.Width), (int)(scale * pixBuf.Height), Gdk.InterpType.Bilinear);
 	        }
-	    });
+	    // });
 	}
 
 	void OnPhotoBoothShutdownRequested (object sender, EventArgs e)
