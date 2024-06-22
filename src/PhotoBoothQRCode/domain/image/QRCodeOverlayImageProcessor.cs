0629feb9d65cc2bfccde9fcc61d314cfc77d0b8c
Patrick Bronneberg <patrick.bronneberg@philips.com>
2024-06-22 10:35:13 +0200

    Add QRCode processing

---
+++ src/PhotoBoothQRCode/domain/image/QRCodeOverlayImageProcessor.cs
@@ -0,0 +1,113 @@
+#region PhotoBooth - MIT - (c) 2014 Patrick Bronneberg
+/*
+  PhotoBooth - an application to control a DIY photobooth
+
+  Permission is hereby granted, free of charge, to any person obtaining
+  a copy of this software and associated documentation files (the
+  "Software"), to deal in the Software without restriction, including
+  without limitation the rights to use, copy, modify, merge, publish,
+  distribute, sublicense, and/or sell copies of the Software, and to
+  permit persons to whom the Software is furnished to do so, subject to
+  the following conditions:
+
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+  
+  Copyright 2014 Patrick Bronneberg
+*/
+#endregion
+
+using com.prodg.photobooth.config;
+using Microsoft.Extensions.Logging;
+using QRCoder;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace com.prodg.photobooth.domain.image
+{
+    /// <summary>
+    /// An image processor processes an image by applying a filter
+    /// </summary>
+    public class QrCodeOverlayImageProcessor : ISingleImageProcessor
+    {
+        private readonly ILogger<QrCodeOverlayImageProcessor> _logger;
+        private readonly string _baseUrl;
+
+        /// <summary>
+        /// C'tor
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="settings"></param>
+        public QrCodeOverlayImageProcessor(ILogger<QrCodeOverlayImageProcessor> logger, ISettings settings)
+        {
+            _logger = logger;
+                   
+            var urlBuilder = new System.Text.StringBuilder();
+            urlBuilder.Append(settings.OffloadAddress.TrimEnd('/'));
+            _baseUrl = urlBuilder.ToString();
+        
+            _logger.LogInformation("QR Code BaseURL configured to URL '{BaseUrl}'", _baseUrl);
+        }
+
+        public Image Process(Image image, string eventId, int sessionIndex)
+        {
+	        if (image == null)
+	        {
+		        throw new ArgumentNullException(nameof(image), "image may not be null");
+	        }
+	        
+	        var overlayImage = PrepareQrCode(eventId, sessionIndex);
+	        if (overlayImage == null)
+	        {
+		        return image;
+	        }
+	        //get a graphics object from the image so we can draw on it
+            
+	        image.Mutate(x =>
+	        {
+		        int xStart = (int) Math.Round((image.Width/2f) - (overlayImage.Width/2f));
+		        int yStart = (int) Math.Round((image.Height/2f) - (overlayImage.Height/2f));
+		        x.DrawImage(overlayImage, new Point(xStart, yStart), 1);
+	        });
+
+	        return image;
+        }
+        
+        /// <summary>
+        /// Processes the images into a single image
+        /// </summary>
+        public Image Process(PhotoSession? session, Image image)
+        {
+	        if (session == null)
+	        {
+		        throw new ArgumentNullException(nameof(session), "session may not be null");
+	        }
+
+	        return Process(image, session.EventId, session.Id);
+        }
+        
+        private Image? PrepareQrCode(string eventId, int sessionIndex)
+        {
+	        var url = $"{_baseUrl}/api/v1/events/{eventId}/sessions/{sessionIndex}";
+	        _logger.LogInformation("PrepareQrCode - {Url}", url);
+	        
+	        var urlPayload = new PayloadGenerator.Url(url);
+	        
+	        try
+	        {
+		        using QRCodeGenerator qrGenerator = new QRCodeGenerator();
+		        using QRCodeData qrCodeData = qrGenerator.CreateQrCode(urlPayload);
+		        using PngByteQRCode qrCode = new PngByteQRCode(qrCodeData);
+		        var qrCodeBytes = qrCode.GetGraphic(20);
+        
+		        return Image.Load<Argb32>(qrCodeBytes);
+	        }
+	        catch (Exception e)
+	        {
+		        _logger.LogError(e, "Error while Preparing QRCode");
+		        return null;
+	        }
+        }
+    }
+}
---
+++ src/PhotoBoothQRCode/infrastructure/hardware/QrCodePrinter.cs
@@ -0,0 +1,35 @@
+﻿using com.prodg.photobooth.domain.image;
+using Microsoft.Extensions.Logging;
+using SixLabors.ImageSharp;
+
+namespace com.prodg.photobooth.infrastructure.hardware;
+
+public class QrCodePrinter: IPrinter
+{
+    private readonly ILogger<QrCodePrinter> _logger;
+    private QrCodeOverlayImageProcessor _processor;
+    private readonly string _baseUrl;
+
+    public QrCodePrinter(ILogger<QrCodePrinter> logger, QrCodeOverlayImageProcessor processor)
+    {
+        _logger = logger;
+        _processor = processor;
+    }
+    
+    public void Initialize()
+    {
+        _logger.LogInformation("Initialize");
+    }
+
+    public void DeInitialize()
+    {
+        _logger.LogInformation("DeInitialize");
+    }
+
+    public void Print(Image image, string eventId, int sessionIndex)
+    {
+        _logger.LogInformation("Print - {0}, {1}", eventId, sessionIndex);
+
+        var mergedImage = _processor.Process(image, eventId, sessionIndex);
+    }
+}
